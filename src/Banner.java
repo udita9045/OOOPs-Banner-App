ac3efe08f6d9d6fd8984f1ad5ed6563102e9f61f
Udita <uditahalder2007@gmail.com>
2026-02-16 23:59:12 +0530

    UC1: Implemented Banner class and display method

---
+++ src/Banner.java
@@ -0,0 +1,11 @@
+public class Banner {
+    String title;
+
+    public Banner(String title) {
+        this.title = title;
+    }
+
+    public void display() {
+        System.out.println("Banner: " + title);
+    }
+}
